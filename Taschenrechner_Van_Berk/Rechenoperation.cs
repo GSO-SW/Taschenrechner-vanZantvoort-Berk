66d6b2a643bac8b1fbb33b104493236fbc4ab5fb
321RORO123 <iah51van_zantvo@gso-koeln.de>
2017-03-15 09:14:11 +0100

    Rechenteilunggrundstein

--- Taschenrechner_Van_Berk/Rechenoperation.cs
+++ Taschenrechner_Van_Berk/Rechenoperation.cs
@@ -9,10 +9,29 @@ namespace Taschenrechner_Van_Berk
     class Rechenoperation
     {
         string Rechnung;
+        int n;
+        bool Rechenteil1;
+        bool Rechenteil2;
+        bool Rechenteil3;
 
         public Rechenoperation(string Übergabe)
         {
             Rechnung = Übergabe;
+            Rechenteil1 = false;
+            Rechenteil2 = false;
+            Rechenteil3 = false;
+        }
+
+        public void Rechnungunterteilen()
+        {
+            do 
+            {
+                //if (Rechnung.Substring())
+                {
+                    Rechenteil1 = true;
+                }
+
+            }while(Rechenteil1 == false);
         }
     }
 }
